3fe890ab07b548c06ce19e359e33e7eec4d6976e
cecilialfo <167810039+cecilialfo@users.noreply.github.com>
2024-05-04 01:15:09 -0300

    atualizando intervalos

--- scripts/criando_indicador_escolaridade.R
+++ scripts/criando_indicador_escolaridade.R
@@ -1,15 +1,15 @@
 # Carregar os dados do arquivo CSV previamente gerado
 dados_sinasc_ <- read.csv2("Base_sinasc_.csv")
 
-# Criar intervalos para a escolaridade da mãe
+# Criar intervalos para a escolaridade da mÃ£e
 dados_sinasc_$intervalo_esc <- cut(dados_sinasc_$ESCMAE, 
-                                  breaks = c(0, 1, 3, 7, 11, Inf), # Por exemplo, dividindo em intervalos de 3 anos
+                                  breaks = c(-Inf, 1, 4, 8, 12, Inf),
                                   labels = c("Nenhuma", "1-3 anos", "4-7 anos", "8-11 anos", "12 e mais"))
 
 # Criar um novo dataframe para armazenar os resultados do indicador
 indicador_escolaridade <- data.frame()
 
-# Calcular as médias de peso, APGAR1 e APGAR5 por intervalo de escolaridade e ano
+# Calcular as mÃ©dias de peso, APGAR1 e APGAR5 por intervalo de escolaridade e ano
 for (ano in unique(dados_sinasc_$ano)) {
   for (intervalo in unique(dados_sinasc_$intervalo_esc)) {
     subset_data <- subset(dados_sinasc_, intervalo_esc == intervalo & ano == ano)
