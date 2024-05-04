35e5249ada050790a99a1e46e27707379d0a6da3
cecilialfo <167810039+cecilialfo@users.noreply.github.com>
2024-05-03 22:39:40 -0300

    Add files via upload

---
+++ scripts/gerando_gráficos.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+
+# Gráfico para peso
+grafico_peso <- ggplot(indicador_escolaridade, aes(x = intervalo_esc, y = media_peso, fill = as.factor(ano))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Média de peso por intervalo de escolaridade da mãe",
+       x = "Intervalo de escolaridade da mãe",
+       y = "Média de peso",
+       fill = "Ano") +
+  scale_fill_manual(values = c("2010" = "skyblue", "2011" = "lightgreen")) +
+  theme_minimal()
+
+# Gráfico para APGAR1
+grafico_apgar1 <- ggplot(indicador_escolaridade, aes(x = intervalo_esc, y = media_apgar1, fill = as.factor(ano))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Média de APGAR1 por intervalo de escolaridade da mãe",
+       x = "Intervalo de escolaridade da mãe",
+       y = "Média de APGAR1",
+       fill = "Ano") +
+  scale_fill_manual(values = c("2010" = "skyblue", "2011" = "lightgreen")) +
+  theme_minimal()
+
+# Gráfico para APGAR5
+grafico_apgar5 <- ggplot(indicador_escolaridade, aes(x = intervalo_esc, y = media_apgar5, fill = as.factor(ano))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Média de APGAR5 por intervalo de escolaridade da mãe",
+       x = "Intervalo de escolaridade da mãe",
+       y = "Média de APGAR5",
+       fill = "Ano") +
+  scale_fill_manual(values = c("2010" = "skyblue", "2011" = "lightgreen")) +
+  theme_minimal()
+
+# Exibir os gráficos
+print(grafico_peso)
+print(grafico_apgar1)
+print(grafico_apgar5)
