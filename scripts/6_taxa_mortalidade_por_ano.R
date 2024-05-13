e7cd39f3fecb5fc6cd20ac51d0c930da7c4d0b25
cecilialfo <167810039+cecilialfo@users.noreply.github.com>
2024-05-13 12:46:01 -0300

    Update and rename 6. taxa_mortalidade_por_ano.R to 6_taxa_mortalidade_por_ano.R

--- scripts/6_taxa_mortalidade_por_ano.R
+++ scripts/6_taxa_mortalidade_por_ano.R
@@ -2,7 +2,7 @@
 
 base_unica <- read.csv2("base_unica.csv", header = TRUE, sep = ",")
 
-# Criando um novo dataframe para armazenar as taxas de mortalidade por ano e município
+# Criando um novo dataframe para armazenar as taxas de mortalidade por ano e municÃ­pio
 taxas_mortalidade_geral <- base_unica %>%
   group_by(Ano, Municipio) %>%
   summarise(Taxa_Mortalidade_Geral = sum(Total) / sum(Populacao) * 1000)
